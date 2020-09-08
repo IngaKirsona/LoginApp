429164e5979290904cc3d7dca0f742c0ee804036
IngaKirsona <ingas@inbox.lv>
2020-09-08 14:24:59 +0300

    1:24

--- LoginApp/Controller/ViewController.swift
+++ LoginApp/Controller/ViewController.swift
@@ -9,12 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var userNameTextField: DesignableTextField!
+    @IBOutlet weak var passwordTextField: DesignableTextField!
+    
+    private let userName = "inga"
+    private let id = "2020"
+    
+    let notificationCenter = NotificationCenter.default
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        notificationCenter.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
-
-
+    
+    @objc func keyboardWillShow(notification: Notification){
+        if self.view.frame.origin.y == 0{
+            self.view.frame.origin.y -= 150
+        }
+    }
+    @objc func keyboardWillHide(notification: Notification){
+        if self.view.frame.origin.y != 0{
+            self.view.frame.origin.y += 150
+        }
+    }
+    
+    
+    @IBAction func loginButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func forgotUserTapped(_ sender: Any) {
+        warningPopUP(withTitle: "Oooops!", withMessage: "Your username is \(userName)")
+    }
+    
+    @IBAction func forgotPasswordTapped(_ sender: Any) {
+        warningPopUP(withTitle: "Oooops!", withMessage: "Your password is \(id)")
+    }
+    //to hide the keyboard
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    
 }
 
