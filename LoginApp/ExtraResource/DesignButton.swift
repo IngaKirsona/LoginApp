cb546e2c5cc42866810135da67e7602fb461510e
IngaKirsona <ingas@inbox.lv>
2020-09-08 12:38:58 +0300

    49:06

--- LoginApp/ExtraResource/DesignButton.swift
+++ LoginApp/ExtraResource/DesignButton.swift
@@ -6,4 +6,21 @@
 //  Copyright © 2020 Inga Kirsona. All rights reserved.
 //
 
-import Foundation
+import UIKit
+@IBDesignable class DesignButton: BounceButton{
+    @IBInspectable var borderWidth: CGFloat = 0.0 {
+        didSet{
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    @IBInspectable var borderColor: UIColor = UIColor.clear {
+        didSet{
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+    @IBInspectable var cornerRadius: CGFloat = 0.0 {
+        didSet{
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+}
